5d21833bae6b70230417ca827f49c74325061591
Jirayu.ninl <Jirayu.ninl@gmail.com>
2022-04-15 01:53:45 +0700

    REMOVE: Next-offline

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const plugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-const withOffline = require('next-offline')
 
 const nextConfig = {
   webpack(config, { webpack, dev, isServer }) {
